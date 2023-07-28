202571289512e01feb369ba8b6586021e21e01b4
kotlasrinivasu <121429003+kotlasrinivasu@users.noreply.github.com>
2023-07-28 15:58:08 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,40 @@
+ReactDOM.render(
+    <div>
+    <h1>this is the nick vujicic biograpy.</h1>,
+    <p>Bachelor of Commerce degreeNick Vujicic, who was born without arms or legs, overcame hardships to become a well-known motivational speaker, coach, and businessman. On December 4, 1982, he was born in Melbourne, Australia. He attended Queensland's Runcorn State High School. Vujicic earned a Bachelor of Commerce degree from Griffith University.</p>
+   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdOEYtjAyeeXnjYZWPRX-NAOS4qQDxpBrvzeQpjZr6u18Y0DziUXoskjBZUqrdXIRvbvU&usqp=CAU" height="250px"width="300px" />
+<ul>
+    <li>Born:Nicholas James Vujicic born in 4 December 1982 (age 40)Melbourne, Victoria, Australia</li>
+    <li>citizenShip:AustraliaUnited States</li>
+    <li>Alma mater:	Griffith University</li>
+    <li>Occupations	:Evangelist. motivational speaker</li>
+    <li>Years active:2004–present</li>
+    <li>wife name:	Kanae Miyahara ​(m. 2012)</li>
+    <li>childrens:4 childrens (2 sons & 2 daughters)</li>
+    <li>officialWebsite:  nickvujicic.com</li>
+
+</ul>
+<h2> number of countries visited</h2>
+<ul>
+    <li>India</li>
+    <li>America</li>
+    <li>London</li>
+    <li>Italy</li>
+    <li>Spain</li>
+    <li>europe</li>
+</ul>
+<h3>Books And Publications </h3>
+<ul>
+    <li>Life Without Limits: Inspiration for a Ridiculously Good Life (2010); ISBN 978-0307589743</li>
+    <li>Your Life Without Limits (2012); ISBN 978-0307731043</li>
+    <li>Limitless: Devotions for a Ridiculously Good Life (2013); ISBN 978-0307730916</li>
+    <li>Unstoppable: The Incredible Power of Faith in Action (2013); ISBN 978-0307730893</li>
+    <li>The Power of Unstoppable Faith (2014); ISBN 978-1601426765</li>
+    <li>Stand Strong (2015); ISBN 978-1601427823</li>
+    <li>Love Without Limits (2016); ISBN 978-1601426185</li>
+<li>Be the Hands and Feet: Living Out God's Love for All His Children, February 13, 2018; ISBN 978-1601426208</li>
+</ul>
+
+    </div>
+    ,
+    document.getElementById("content")); 
